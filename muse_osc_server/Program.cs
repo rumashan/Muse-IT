bb2305ad25d6208ecf0150ef6d79fccf86de1db0
Rashmi Umashankar <rashmiumashankar@Rashmis-MacBook-Pro.local>
2017-01-29 11:15:18 -0500

    files

---
+++ muse_osc_server/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using SharpOSC;
+
+namespace muse_osc_server
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			// Callback function for received OSC messages. 
+			// Prints EEG and Relative Alpha data only.
+			HandleOscPacket callback = delegate(OscPacket packet)
+			{
+				var messageReceived = (OscMessage)packet;
+				var addr = messageReceived.Address;
+				if(addr == "/muse/eeg") {
+					Console.Write("EEG values: ");
+					foreach(var arg in messageReceived.Arguments) {
+						Console.Write(arg + " ");
+					}
+				}
+				if(addr == "/muse/elements/alpha_relative") {
+					Console.Write("Relative Alpha power values: ");
+					foreach(var arg in messageReceived.Arguments) {
+						Console.Write(arg + " ");
+					}
+				}
+			};
+
+			// Create an OSC server.
+			var listener = new UDPListener(5000, callback);
+
+			Console.WriteLine("Press enter to stop");
+			Console.ReadLine();
+			listener.Close();
+		}
+	}
+}
